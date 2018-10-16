0072eb0a27af7f6f315a6b69fc11a8b6bd618053
jezbread <jezreelcallejas@jezbreads-MacBook-Pro.local>
2018-10-16 08:06:27 -0700

    Started basic setup of html and js files, as well as staged

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // Write your JavaScript here
+
+function calculateChange(due, given) {
+    due = document.getElementById('ammount-due').value;
+    given = document.getElementById('ammount-given').value;
+
+    let cents, quarters, dimes, nickels, pennies;
+
+
+}
+
+getElementById('calculate-change').addEventListener('click',calculateChange(due, given));
